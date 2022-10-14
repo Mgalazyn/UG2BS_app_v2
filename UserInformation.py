a88e342ebf2427a1cc37fb5a875c38ef21a06b3a
Mgalazyn <m.galazynn@gmail.com>
2022-10-14 21:33:31 +0200

    Adding userinfo, calories intake calculator

--- UserInformation.py
+++ UserInformation.py
@@ -5,9 +5,19 @@ class UserInformation:
         self.age = int(input('How old are you?: '))
         self.height = int(input('How tall are you(in cm)?:  '))
         self.weight = int(input('What\'s your weight(in kg)?: '))
+        self.header = 'USER INFO'
 
     def __str__(self):
         return (f' User information: First name: {self.first_name}, Last name: {self.last_name}, Age: {self.age}'
               f' Height: {self.height}, Weight: {self.weight}')
 
+    def save_info_to_csv_folder(self):
+        with open('User_info.csv', 'a') as f:
+            f.write(f" {self.header} \n"
+                    f"User's name is {self.first_name} \n"
+                    f"User's surname is {self.last_name} \n"
+                    f"User's age is {self.age} \n"
+                    f"User's height is {self.height} cm \n"
+                    f"User's weight is {self.weight}  kg\n "
+                    f"\n")
 
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from UserInformation import UserInformation
 from calories_intake import CalculateCaloriesIntake
+import pandas as pd
 
 
 def run_example():
-    calories = CalculateCaloriesIntake()
-    calories.daily_calories_intake()
+    p = UserInformation()
+    p.save_info_to_csv_folder()
+    fi = pd.read_csv('User_info.csv')
+    print(fi)
 
 
 if __name__ == '__main__':
