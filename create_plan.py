a38ea04acbb04587a4882178ab00624f9f235b4c
Mgalazyn <m.galazynn@gmail.com>
2022-10-17 22:29:50 +0200

    Adding main_lifts, class create plan

--- create_plan.py
+++ create_plan.py
@@ -6,4 +6,7 @@ class CreatePlan:
         self.main_lift = main_lift
         self.additional_exercises = additional_exercises
         self.training_days = int(input('How many day a week would you like to train?: '))
-        self.traning_plan = [] * self.traning
+        self.traning_plan = [] * self.traning_days
+
+    # def add_main_lift(self):
+
